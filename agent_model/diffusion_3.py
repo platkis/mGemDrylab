5145b8a329193a8f6bb4bd30837c7a065d1a94b7
ianfare <apellius@gmail.com>
2016-08-29 16:33:10 -0400

    Including modules for making gifs

--- agent_model/diffusion_3.py
+++ agent_model/diffusion_3.py
@@ -1,6 +1,10 @@
+# Must have Pillow installed
+
+import images2gif
 import numpy as np
 from numpy.random import *
 from matplotlib.pyplot import *
+from PIL import Image,ImageSequence
 dt=0.002;
 Dz=1;
 dx=1;
@@ -15,7 +19,6 @@ p=np.zeros([nx,ny,2]);
 for f in range(20):
   p[round((nx-1)*rand()),round((ny-1)*rand()),0]=np.sign(rand()-0.51);
 for m in range(1,nt):
-  print(m)
 #A simple implementation (but not quite eficient) will be iterating each point at a time. Because we have a matrix, we can operate with whole sections of the matrix at each time.
 #Basically, we take time slices and operate them as a whole. To use centered differences, we simply shift the matrix one element in the x direction or in the y direction.
   
@@ -27,6 +30,8 @@ for m in range(1,nt):
     p[1:nx-1,1:ny-1,1]=p[1:nx-1,1:ny-1,0]+dt*Dz*((p[2:nx,1:ny-1,0]-2*p[1:nx-1,1:ny-1,0]+p[0:nx-2,1:ny-1,0])/np.power(dx,2)+(p[1:nx-1,2:ny,0]-2*p[1:nx-1,1:ny-1,0]+p[1:nx-1,0:ny-2,0])/np.power(dy,2));
 
   if m % 100 == 0:
+    print(m)
+  if m % 10 == 0:
     #Finally we plot several iterations
     fig = figure()
     for g in range(18):
@@ -35,3 +40,4 @@ for m in range(1,nt):
     pcolor(p[:,:,1],vmin=-0.001,vmax=0.001,cmap='jet')
     axis('off')
     savefig("./img/step"+str(m)+".png",bbox_inches="tight",pad_inches=0)
+    close()
